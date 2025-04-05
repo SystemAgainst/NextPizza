5ccaec0f1fa1d201006dc17d07755d9d19f5d3f3
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2025-04-05 17:22:23 +1000

    feat: code brush

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import { Container, Title } from "@/components/shared";
 
 export default function Home() {
   return (
-      <div className="flex justify-center items-center">
+      <>
         <Container className="mt-10">
             <Title text="Все пиццы" size="lg" className="font-extrabold" />
         </Container>
-      </div>
+      </>
   );
 }
