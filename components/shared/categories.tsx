8e0dda32b54a3316d0557b72fe8ee03dc292b84d
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2025-04-05 17:34:26 +1000

    feat: add component categories

---
+++ components/shared/categories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {cn} from "@/lib/utils";
+
+interface Props {
+    className?: string;
+}
+
+const categories = ['Пиццы', 'Комбо', 'Закуски', 'Коктейли', 'Кофе', 'Напитки', 'Десерты', 'Десерты'];
+const activeCategory = 0;
+
+export const Categories: React.FC<Props> = ({ className }) => {
+    return (
+        <div className={cn('inline-flex gap-1 bg-gray-50 rounded-2xl p-1', className)}>
+            {categories.map((category, index) => (
+                <a className={cn(
+                    'flex items-center font-bold h-11 rounded-2xl px-5',
+                    activeCategory === index && 'bg-white shadow-md shadow-gray-200 text-primary'
+                )}
+                   key={index}>
+                    <button>{category}</button>
+                </a>
+            ))}
+        </div>
+    );
+};
