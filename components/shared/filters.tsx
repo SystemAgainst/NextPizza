3e7a891ea57c3bcdfb95172a81329da0816422e6
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2025-04-05 18:31:57 +1000

    feat: add component part for price and make custom range slider

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 import { Title } from "./title";
-import {FilterCheckbox} from "@/components/shared/filter-checkbox";
+import { FilterCheckbox } from "@/components/shared";
+import { Input, RangeSlider } from "@/components/ui";
 
 interface Props {
     className?: string;
@@ -14,6 +15,15 @@ export const Filters: React.FC<Props> = ({ className }) => {
             <div className="flex flex-col gap-4">
                 <FilterCheckbox text="Можно собирать" value="1" />
                 <FilterCheckbox text="Новинки" value="2" />
+            </div>
+
+            <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+                <p className="font-bold mb-3">Цена от и до:</p>
+                <div className="flex gap-3 mb-5">
+                    <Input type="number" placeholder="0" min={0} max={30000} defaultValue={0} />
+                    <Input type="number" min={100} max={30000} placeholder="30000" />
+                </div>
+                <RangeSlider min={0} max={5000} step={10} value={[0, 5000]} />
             </div>
         </div>
     );
