5abcacc389979e4c77334c5c7fe1e6732c3cd77a
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2025-04-06 14:57:15 +1000

    feat: add products-group-list

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import {
     Title,
     Filters,
     ProductCard,
+    ProductsGroupList,
 } from "@/components/shared";
 
 export default function Home() {
@@ -22,11 +23,8 @@ export default function Home() {
 
                 <div className="flex-1">
                     <div className="flex flex-col gap-16">
-                        <ProductCard
-                            name="Чизбургер-пицца"
-                            price={550}
-                            imageUrl="https://media.dodostatic.net/image/r:584x584/0194d4fd39bb7352bfa5de2219e88b9b.avif"
-                        />
+                        <ProductsGroupList title="Пиццы" items={[1, 2, 3, 4, 5]} />
+                        <ProductsGroupList title="Комбо" items={[1, 2, 3, 4, 5]} />
                     </div>
                 </div>
             </div>
