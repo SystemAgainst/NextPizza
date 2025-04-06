cac2ca737dc0fd17f9b5020021e3635b5f65fd3a
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2025-04-06 12:44:28 +1000

    feat: add input for filters and filter-process

--- components/shared/checkbox-filters-group.tsx
+++ components/shared/checkbox-filters-group.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FC } from "react";
+import {ChangeEvent, FC, useMemo, useState} from "react";
 import { FilterCheckbox, FilterCheckboxProps } from "./filter-checkbox";
 import { Input } from "../ui";
 
@@ -9,7 +9,7 @@ type Item = FilterCheckboxProps;
 interface Props {
     title: string;
     items: Item[];
-    defaultItems?: Item[];
+    defaultItems: Item[];
     limit?: number;
     searchInputPlaceholder?: string;
     onChange?: (value: string[]) => void;
@@ -29,16 +29,45 @@ export const CheckboxFiltersGroup: FC<Props> = (props) => {
         className,
     } = props;
 
+    const [expanded, setExpanded] = useState<boolean>(false);
+    const [searchValue, setSearchValue] = useState<string>('');
+
+    const handleExpandedButton = () => setExpanded((prev) => !prev);
+
+    const onChangeSearchInput = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(e.target.value);
+    };
+
+    const filteredItems = useMemo(() => {
+        const query = searchValue.toLowerCase();
+
+        return items.filter((item) => item.text.toLowerCase().includes(query));
+    }, [items, searchValue]);
+
+    const collapsedList = useMemo(() => {
+        if (!expanded) return defaultItems?.slice(0, limit);
+
+        return filteredItems;
+    }, [expanded, defaultItems, filteredItems, limit]);
+
+
     return (
         <div className={className}>
             <p className="font-bold mb-3">{title}</p>
 
-            <div className="mb-5">
-                <Input placeholder={searchInputPlaceholder} className="bg-gray-50 border-none" />
-            </div>
+            {expanded && (
+                <div className="mb-5">
+                    <Input
+                        onChange={onChangeSearchInput}
+                        placeholder={searchInputPlaceholder}
+                        className="bg-gray-50 border-none"
+                    />
+                </div>
+            )}
+
 
             <div className="flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar">
-                {items.map((item, index) => (
+                {collapsedList.map((item, index) => (
                     <FilterCheckbox
                         key={index}
                         text={item.text}
@@ -49,6 +78,14 @@ export const CheckboxFiltersGroup: FC<Props> = (props) => {
                     />
                 ))}
             </div>
+
+            {items.length > limit && (
+                <div className={expanded ? 'border-t border-t-neutral-100 mt-4' : ''}>
+                    <button onClick={handleExpandedButton} className="text-primary mt-3 cursor-pointer">
+                        {expanded ? 'Скрыть' : '+ Показать все'}
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
